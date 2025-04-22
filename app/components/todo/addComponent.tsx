b6be9104aa0a65b73995fa6cfef334765171658b
zerock <kangckck24@gmail.com>
2025-04-22 11:34:09 +0900

    11AAA

--- app/components/todo/addComponent.tsx
+++ app/components/todo/addComponent.tsx
@@ -6,18 +6,19 @@ export default function TodoAddComponent () {
             <div>
                 <label htmlFor="title" className="block text-sm font-medium text-gray-700 mb-1">Title</label>
                 <input type="text" id="title" name="title" placeholder="제목을 입력하세요"
-                       className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                       className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required={true}/>
             </div>
 
             <div>
                 <label htmlFor="writer" className="block text-sm font-medium text-gray-700 mb-1">Writer</label>
                 <input type="text" id="writer" name="writer" placeholder="작성자 이름"
-                       className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                       className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" required={true}/>
             </div>
             <div>
                 <label htmlFor="dueDate" className="block text-sm font-medium text-gray-700 mb-1">Due Date</label>
                 <input type="date" id="dueDate" name="dueDate"
-                       className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                       className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" required={true}/>
             </div>
             <div className="pt-4">
                 <button type="submit"
