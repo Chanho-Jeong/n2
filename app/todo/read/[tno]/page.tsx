95869d293004b65cd34b861bb48c94946a520a88
zerock <kangckck24@gmail.com>
2025-04-22 10:26:08 +0900

    11

--- app/todo/read/[tno]/page.tsx
+++ app/todo/read/[tno]/page.tsx
@@ -4,8 +4,14 @@
 async function TodoReadPage({params}: {params:Promise<any>}) {
 
     const param = await params
+    const tno = param.tno
 
-    console.log(param)
+    //tno를 이용해서 데이터 패치
+    const res = await fetch(`http://localhost:8080/api/todo/${tno}`)
+    const data = await res.json()
+
+    console.log(data)
+
 
     return (
         <div>
